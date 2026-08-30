98775a3ea8a2d9d61af35112f41f22e5c512845d
Ayush <176569369+Ayush18102002@users.noreply.github.com>
2026-08-30 20:43:59 +0530

    LLMs core

---
+++ transformers/main2.py
@@ -0,0 +1,16 @@
+"""
+TEXT GENERATION INFERENCE WITH LLM
+-> it is a process of using a trained LLm to generate human like text from a given input prompt.
+
+-> role attention: is what give LLms their abilities to understand context and generate coherent responses. 
+                   when predicting the next word, not every word in a sentence carries wqual weight.
+
+-> context length and attention span: The context length refers to the maximum number of tokens (words or parts of words) that the LLM can process at once. 
+                                      Think of it as the size of the model’s working memory.
+
+-> the art of prompting: Careful design of the prompt makes it easier to guide the generation of the LLM toward the desired output.
+
+-> the two phase of inference process : 
+
+
+"""
