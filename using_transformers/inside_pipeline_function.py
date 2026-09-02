859029a64991fd21678d0a8b4bdd64f1a4c28003
Ayush <176569369+Ayush18102002@users.noreply.github.com>
2026-09-02 11:48:33 +0530

    inside  pipeline function complete

--- using_transformers/inside_pipeline_function.py
+++ using_transformers/inside_pipeline_function.py
@@ -6,8 +6,8 @@ results = classifier([
     
     I've been waiting for a HuggingFace course my whole life.",
         "I hate this so much!",
-
-])
+]
+)
 print(results)
 
 """
@@ -38,4 +38,92 @@ from transformers import AutoTokenizer
 checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
+"""
 
+Once we have the tokenizer, we can directly pass our sentences to it and we’ll get back a dictionary that’s ready to feed to our model! The only thing left to do is to convert the list of input IDs to tensors.
+
+You can use 🤗 Transformers without having to worry about which ML framework is used as a backend; it might be PyTorch or Flax for some models. However, Transformer models only accept tensors as input. If this is your first time hearing about tensors, you can think of them as NumPy arrays instead. A NumPy array can be a scalar (0D), a vector (1D), a matrix (2D), or have more dimensions. It’s effectively a tensor; other ML frameworks’ tensors behave similarly, and are usually as simple to instantiate as NumPy arrays.
+
+To specify the type of tensors we want to get back (PyTorch or plain NumPy), we use the return_tensors argument:
+
+
+"""
+raw_inputs = [
+    "I've been waiting for a HuggingFace course my whole life.",
+    "I hate this so much!",
+]
+
+inputs = tokenizer(raw_inputs, return_tensors="pt", padding=True , truncation=True)
+print(inputs)
+
+"""
+{'input_ids': tensor([[  101,  1045,  1005,  2310,  2042,  3403,  2005,  1037, 17662, 12172,
+          2607,  2026,  2878,  2166,  1012,   102],
+        [  101,  1045,  5223,  2023,  2061,  2172,   999,   102,     0,     0,
+             0,     0,     0,     0,     0,     0]
+             ]), 
+  'token_type_ids': tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]), 
+
+   'attention_mask': tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]])}
+
+
+        The output itself is a dictionary containing two keys, input_ids and attention_mask. input_ids contains two rows of integers (one for each sentence) that are the unique identifiers of the tokens in each sentence. We’ll explain what the attention_mask is later in this chapter.
+
+
+"""
+
+# Going through the model
+# We can download our pretrained model the same way we did with our tokenizer. 🤗 Transformers provides an AutoModel class which also has a from_pretrained() method:
+
+
+
+from transformers import AutoModel
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModel.from_pretrained(checkpoint)
+
+
+# high dimensional vector
+# the vector output by the model is a high dimensional vector, which is not very useful for us. We need to convert it into a more human-readable format. To do this, we can use a classification head, which is a small neural network that takes the high dimensional vector and outputs a probability distribution over the possible classes (in our case, positive or negative sentiment).
+# it is usually large it generally has 3 dimension
+# batch Size :  The number of sequences processed at a time 
+# Sequence length : The length of the numerical representation of the sequence 
+# Hidden size : The vector dimension of each model input.
+
+outputs  = model(**inputs)
+print(outputs.last_hidden_state.shape)
+
+# torch.Size([2, 16, 768])
+"""
+Model heads: Making sense out of numbers
+The model heads take the high-dimensional vector of hidden states as input and project them onto a different dimension. They are usually composed of one or a few linear layers:
+
+
+"""
+
+from transformers import AutoModelForSequenceClassification
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+outputs = model(**inputs)
+
+print(outputs.logits.shape)
+
+# torch.Size([2, 2])
+
+print(outputs.logits)
+
+# tensor([[-1.5607,  1.6123],
+#         [ 4.1692, -3.3464]], grad_fn=<AddmmBackward0>)
+
+
+
+import torch 
+predection = torch.nn.functional.softmax(outputs.logits, dim=-1)
+print(predection)
+
+# tensor([[4.0195e-02, 9.5980e-01],
+#        [9.9946e-01, 5.4418e-04]], grad_fn=<SoftmaxBackward0>)
+ 
+
+ 
