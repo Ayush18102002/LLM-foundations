af1b7d5d8734e392d3cf4fda2e0cafc6fec36192
Ayush <176569369+Ayush18102002@users.noreply.github.com>
2026-09-08 15:49:48 +0530

    fine tuning a model with the trainer API

---
+++ fine_tuning_pre-trained_model/fine-tuning_with_api.py
@@ -0,0 +1,78 @@
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer
+)
+import evaluate
+import numpy as np
+
+
+# 1. Dataset
+raw_datasets = load_dataset("glue", "mrpc")
+
+
+# 2. Model name
+checkpoint = "bert-base-uncased"
+
+
+# 3. Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+# 4. Tokenize
+def tokenize_function(example):
+    return tokenizer(
+        example["sentence1"],
+        example["sentence2"],
+        truncation=True
+    )
+
+tokenized_datasets = raw_datasets.map(
+    tokenize_function,
+    batched=True
+)
+
+
+# 5. Model
+model = AutoModelForSequenceClassification.from_pretrained(
+    checkpoint,
+    num_labels=2
+)S
+
+
+# 6. Training settings
+training_args = TrainingArguments(
+    "test-trainer",
+    eval_strategy="epoch"
+)
+
+
+# 7. Metrics
+metric = evaluate.load("glue", "mrpc")
+
+def compute_metrics(eval_preds):
+    logits, labels = eval_preds
+
+    predictions = np.argmax(logits, axis=-1)
+
+    return metric.compute(
+        predictions=predictions,
+        references=labels
+    )
+
+
+# 8. Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    processing_class=tokenizer,
+    compute_metrics=compute_metrics
+)
+
+
+# 9. Train!
+trainer.train()
