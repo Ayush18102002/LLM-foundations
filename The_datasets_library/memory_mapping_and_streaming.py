6a8eefc9e5a7179fe432c7777058416250ac11c6
Ayush <176569369+Ayush18102002@users.noreply.github.com>
2026-09-12 19:54:27 +0530

    memory mapping and streaming - pile

---
+++ The_datasets_library/memory_mapping_and_streaming.py
@@ -0,0 +1,8 @@
+"""
+Datasets has been designed to overcome these limitations. It frees you from memory management problems by treating datasets as memory-mapped files, and from hard drive limits by streaming the entries in a corpus.
+
+Datasets has been designed to overcome these limitations. It frees you from memory management problems by treating datasets as memory-mapped files, and from hard drive limits by streaming the entries in a corpus.
+"""
+
+# pile :-
+
